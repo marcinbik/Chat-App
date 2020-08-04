85eee567d4c246167d87c1136b85083c475d4a7e
marcinbik <marcinbik55@gmail.com>
2020-08-05 00:01:32 +0200

    add gitignore files and remove node_modules dir2

--- client/src/components/UserList/UserList.js
+++ client/src/components/UserList/UserList.js
@@ -1,29 +1,25 @@
-import React from 'react';
-import onlineIcon from '../../icons/onlineIcon.png';
-import Styles from '../UserList/UserList.module.scss';
+import React from "react";
+import onlineIcon from "../../icons/onlineIcon.png";
+import Styles from "../UserList/UserList.module.scss";
 
-const UserList = ({userList}) => (
-
-<div className={Styles.textContainer}>
-  {
-    userList
-    ?(
-      <div>
+const UserList = ({ userList }) => (
+  <div className={Styles.userList}>
+    {userList ? (
+      <div className={Styles.textContainer}>
         <h1>People currently chatting</h1>
-        <div className={Styles.activeContainer}></div>
-      <h2>
-        {userList.map(({name})=>
-        <div key={name} className={Styles.activeItem}>
-          {name}
-          <img src={onlineIcon} alt="Online Icon"/>
-        </div>)}
-      </h2>
+        <div>
+          <h2>
+            {userList.map(({ name }) => (
+              <div key={name} className={Styles.activeItem}>
+                {name}
+                <img src={onlineIcon} alt="Online Icon" />
+              </div>
+            ))}
+          </h2>
+        </div>
+        <br/>
       </div>
-    )
-    : null
-  }
-</div>
-
-
-)
+    ) : null}
+  </div>
+);
 export default UserList;
